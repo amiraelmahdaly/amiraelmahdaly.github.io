1a5d61eca408f8222a735a1984a1df0d48e4d1d6
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2017-12-22 14:58:09 +0200

    any

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 echo off
-xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html /s/h/e/k/f/c/y
+xcopy  "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html" /s/h/e/k/f/c/y
 xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.css E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.css /s/h/e/k/f/c/y
 xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.js E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.js /s/h/e/k/f/c/y
 git add --all
