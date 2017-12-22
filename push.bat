b7eef2cba0d66f3d6cf609566c87faa7e33e1d44
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2017-12-22 15:02:42 +0200

    any

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 echo off
-xcopy "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html" /h/e/k/f/c/y
-xcopy "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.css" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.css" /h/e/k/f/c/y
-xcopy "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.js" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.js" /h/e/k/f/c/y
+xcopy /s "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html" /Y
+xcopy /s "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.css" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.css" /Y
+xcopy /s "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.js" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.js" /Y
 git add --all
 git commit -m "any"
 git push
