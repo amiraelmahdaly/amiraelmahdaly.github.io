5de111754581c33f8f7c7219a58de33e14a0e2d2
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2017-12-22 14:53:48 +0200

    any

---
+++ push.bat
@@ -0,0 +1,8 @@
+echo off
+xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html /s/h/e/k/f/c/y
+xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.css E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.css /s/h/e/k/f/c/y
+xcopy  E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.js E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.js /s/h/e/k/f/c/y
+git add --all
+git commit -m "any"
+git push
+set /p delExit=Press the ENTER key to exit...:
