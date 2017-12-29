1933fbf531cd60bf4061fa17b9a62986f06a6f36
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2017-12-29 20:23:29 +0200

    Delete push.bat

--- push.bat
+++
@@ -1,8 +0,0 @@
-echo off
-xcopy  "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.html" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.html" /Y
-xcopy  "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.css" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.css" /Y
-xcopy  "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\Home.js" "E:\Projects\Office Addins\ClaimMirroringAddin\ClaimMirroringAddinWeb\amiraelmahdaly.github.io\ClaimMirroringAddin\Home.js" /Y
-git add --all
-git commit -m "any"
-git push
-set /p delExit=Press the ENTER key to exit...:
