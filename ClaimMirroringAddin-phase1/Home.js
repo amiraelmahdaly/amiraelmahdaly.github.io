9af995277981781d1df8005716179816696690bb
amira elmahdaly <amira.elmahdaly@yahoo.com>
2017-12-29 21:04:21 +0200

    First Phase

---
+++ ClaimMirroringAddin-phase1/Home.js
@@ -0,0 +1,29 @@
+﻿
+
+(function () {
+    "use strict";
+
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+      
+    };
+
+
+    $(document).ready(function () {
+
+        function validate(id) {
+            setTimeout(function () {
+                $("#" + id).removeClass("onclic");
+                $("#" + id).addClass("validate", callback(id));
+            }, 2250);
+        }
+        function callback(id) {
+            setTimeout(function () {
+                $("#" + id).removeClass("validate");
+            }, 1250);
+        }
+
+    });
+
+})();
