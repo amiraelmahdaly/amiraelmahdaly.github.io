2845782d03028b122ae46bf0a29a5fba15c5a072
amira elmahdaly <amira.elmahdaly@yahoo.com>
2017-12-31 19:12:34 +0200

    Phase 2

--- ClaimMirroringAddin-phase1/Home.js
+++ ClaimMirroringAddin-phase1/Home.js
@@ -13,9 +13,89 @@
     $(document).ready(function () {
 
 
+        function AddPreamble() {
+            Word.run(function (context) {
+                var pars = context.document.getSelection().paragraphs;
+                pars.load();
+                return context.sync().then(function () {
+
+                    Claim.Preamble.Style = pars.items[0].style;
+                    Claim.Preamble.LineSpacing = pars.items[0].lineSpacing;
+
+                    return context.sync();
+                })
+            }).catch(function (error) {
+                console.log(error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
+
+
+        }
+        function AddStep() {
+            Word.run(function (context) {
+                var IsStart = false;
+                var pars = context.document.getSelection().paragraphs;
+                pars.load();
+                return context.sync().then(function () {
+                    for (var i = 0; i < pars.items.length; i++) {
+                        if (i == 0) IsStart = true;
+                        else IsStart = false;
+
+                        Claim.Steps.push({
+                            "Text": pars.items[i].text, "Style": pars.items[i].style, "LineSpacing": pars.items[i].lineSpacing, "IsStart": IsStart,
+                            "FirstLineIndent": pars.items[i].firstLineIndent, "LeftIndent": pars.items[i].leftIndent
+
+                        });
+                    }
+
+                    return context.sync();
+                })
+            }).catch(function (error) {
+                console.log(error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
+
+        }
+        function RemoveIng(text) {
+            var verbWithoutIng = text.split(" ")[0].slice(0, -3) + " ";
+            var textWithoutVerb = text.substring(text.indexOf(" ") + 1, text.length);
+            return verbWithoutIng + textWithoutVerb;
+
+        }
+        function ProcessStep(type, text) {
+
+            switch (type) {
+                case "Device":
+                    return RemoveIng(text);
+                    break;
+                case "Apparatus":
+                    return "means for " + text;
+                    break;
+                case "CRM":
+                    return RemoveIng(text);
+                    break;
+
+            }
+
+        }
+        var Claim = {
+            "Preamble": { "Style": "", "LineSpacing": "" },
+            "Steps": []
+        };
+
+
+        var DevicePreamble = "A device for wireless communication, comprising: memory; and one or more processors coupled to the memory, the memory and the one or more processors configured to:";
+        var ApparatusPreamble = "An apparatus for wireless communication, comprising:";
+        var CRMPreamble = "A non-transitory computer-readable medium storing one or more instructions for wireless communication, the one or more instructions comprising: one or more instructions that, when executed by one or more processors of a device, cause the one or more processors to:";
+
         $(".button").click(function (event) {
             $(this).addClass("onclic", validate(event.target.id));
-           
+            if (event.target.id == "btnPreamble") AddPreamble();
+            else if (event.target.id == "btnStep") AddStep();
         });
 
 
