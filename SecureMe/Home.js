76685789fb9f3bb4cf1ce3c132b7bb493e97e647
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2018-06-05 07:05:00 +0200

    Happy Path

--- SecureMe/Home.js
+++ SecureMe/Home.js
@@ -4,12 +4,10 @@
   var messageBanner;
 
   // The Office initialize function must be run each time a new page is loaded.
-  Office.initialize = function (reason) {
-
-  };
+  
 
 
-    var app = angular.module('myApp', []);
+    var app = angular.module('myApp', ['ngSanitize']);
     app.directive('onFinishRender', function ($timeout) {
         return {
             restrict: 'A',
@@ -23,23 +21,60 @@
             }
         }
     });
+
+
+ 
     app.controller('myCtrl', function ($scope, $http, $compile) {
 
+
+        var mail = "";
         var rawToken = "";
         var restId = '';
+        
         var restUrl = '';
-        var mail = "";
 
-        $("#btnTest").click(function(){
-            loadRestDetails(event);
-        });
-        $(document).ready(function () {
-            var element = document.querySelector('.ms-MessageBanner');
-            messageBanner = new fabric.MessageBanner(element);
-            messageBanner.hideBanner();
-           // loadRestDetails(event);
-        });
+        var currentMessageID = "";
 
+
+        Office.initialize = function (reason) {
+            $(document).ready(function () {
+                var element = document.querySelector('.ms-MessageBanner');
+                messageBanner = new fabric.MessageBanner(element);
+                messageBanner.hideBanner();
+                loadRestDetails();
+
+            });
+        };
+
+        function loadRestDetails() {
+
+
+
+            mail = Office.context.mailbox.userProfile.emailAddress;
+            if (Office.context.mailbox.diagnostics.hostName !== 'OutlookIOS') {
+                restId = Office.context.mailbox.convertToRestId(
+                    Office.context.mailbox.item.itemId,
+                    Office.MailboxEnums.RestVersion.Beta
+                );
+            } else {
+                restId = Office.context.mailbox.item.itemId;
+            }
+            restUrl = Office.context.mailbox.restUrl + '/v2.0/me/';
+            Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
+                if (result.status === "succeeded") {
+                    rawToken = result.value;
+
+                    getItemHeadersViaRest();
+                } else {
+                    rawToken = 'error';
+                }
+            });
+
+
+
+
+
+        }
         //guage
         var opts = {
             lines: 10, // The number of lines to draw
@@ -71,36 +106,14 @@
 
         var target = $("#guageCanvas")[0];
         var g = new Gauge(target).setOptions(opts); // create sexy gauge!
-            g.maxValue = 800; // set max gauge value
-            g.animationSpeed = 20; // set animation speed (32 is default value)
-            g.set(300); // set actual value
-      
+        g.maxValue = 800; // set max gauge value
+        g.animationSpeed = 20; // set animation speed (32 is default value)
+        g.set(300); // set actual value
+
         //end guage
-        function loadRestDetails(event) {
-           
 
-            mail = Office.context.mailbox.userProfile.emailAddress;
-            if (Office.context.mailbox.diagnostics.hostName !== 'OutlookIOS') {
-                restId = Office.context.mailbox.convertToRestId(
-                    Office.context.mailbox.item.itemId,
-                    Office.MailboxEnums.RestVersion.Beta
-                );
-            } else {
-                restId = Office.context.mailbox.item.itemId;
-            }
-            restUrl = Office.context.mailbox.restUrl + '/v2.0/me/';
-            Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
-                if (result.status === "succeeded") {
-                    rawToken = result.value;
-    
-                    getItemHeadersViaRest(event);
-                } else {
-                    rawToken = 'error';
-                }
-            });
-
-        }
         function sendApiData(recipient, headerMessage) {
+
             var settings = {
                 "async": true,
                 "crossDomain": true,
@@ -110,16 +123,66 @@
                     "Content-Type": "application/json",
                 },
                 "processData": false,
+                // "data": "{\r\n\"apiKey\" : \"23424-324234-XX3453-ASDA89\",\r\n\"type\" : \"PhishingButtonPressed\",\r\n\"recipient\" : \"" + recipient + "\",\r\n\"headerFound\" : \"true\", \r\n\"headerMessage\" : \"" + headerMessage + "\"" + "}"
                 "data": "{\r\n\"apiKey\" : \"23424-324234-XX3453-ASDA89\",\r\n\"type\" : \"PhishingButtonPressed\",\r\n\"recipient\" : \"" + recipient + "\",\r\n\"headerFound\" : " + (headerMessage !== "notFound" ? "\"true\", \r\n\"headerMessage\" : \"" + headerMessage + "\"" : "\"false\"") + "}"
             }
             var x = "";
             $.ajax(settings).done(function (response) {
                 console.log(response);
-            }).fail(function (error) {
-                console.log(error);
-            });
+                PopualateData(response);
+
+
+                $("#spinnerCon").css("display", "none");
+            }).fail(errorHandler)
         }
-        function getItemHeadersViaRest(event) {
+
+        $("#btnForward").click(function () {
+            forwardMessage(currentMessageID, $scope.response.forwardEmail);
+        });
+        $("#btnMoveToPhishing").click(function () {
+           CreatePhishingFolder(currentMessageID);
+
+        });
+        function PopualateData(response) {
+            response = JSON.parse(response);
+            $scope.response = {
+                "logoURL": response.LogoURL,
+                "essScore": response.EssScore,
+                "sections": response.Response,
+                "enableForward": (response.EnableForward == "true") ? true : false,
+                "forwardEmail": response.ForwardEmail
+            };
+
+            var HtmlSections="";
+            var sections = $scope.response.sections;
+            for (var i = 0; i < sections.length; i++) {
+                switch (sections[i].Type.toLowerCase()) {
+
+                    case "seperator":
+                        HtmlSections += GenerateSeparator(sections[i].Properties.Lines);
+                        break;
+                    case "text":
+                        HtmlSections += GenerateText(sections[i].Properties.Text, sections[i].Properties.Justify);
+                        break;
+
+                    case "link":
+                        HtmlSections += GenerateLink(sections[i].Properties.Href, sections[i].Properties.Text, sections[i].Properties.Justify);
+                        break;
+
+
+                    default:
+                }
+            }
+            $("#sections").html(HtmlSections);
+            g.set($scope.response.essScore);
+            $scope.$apply();
+        
+        }
+        function Request(settings, done, fail) {
+
+            $.ajax(settings).done(done).fail(fail);
+        }
+        function getItemHeadersViaRest() {
 
             $.ajax({
                 url: restUrl + 'messages/' + restId + '?$select = InternetMessageHeaders',
@@ -129,32 +192,29 @@
                     'Authorization': 'Bearer ' + rawToken
                 }
             }).done(function (item) {
+                currentMessageID = item.Id;
                 var headersNames = item.InternetMessageHeaders.map(function (el) {
                     return el.Name.toLowerCase();
                 });
                 var index = $.inArray("x-sn-email-phishing", headersNames);
                 if (index == -1) {
+                   
                     sendApiData(mail, "notFound");
-                    forwardMessage(item.Id);
+                  
                 }
                 else {
-                    Office.context.mailbox.item.notificationMessages.addAsync("progress", {
-                        type: "progressIndicator",
-                        message: "Good Job, you caught a phish 👍"
-                    });
+                    // ShowBarNotification("progress", "progressIndicator", "Good Job, you caught a phish 👍");
+                    showNotification("Good Job, you caught a phish 👍");
+                    //event.completed();
                     sendApiData(mail, item.InternetMessageHeaders[index].Value);
 
                 }
-                CreatePhishingFolder(item.Id, event);
-            }).fail(function (error) {
-                console.log(error);
-                Office.context.mailbox.item.notificationMessages.addAsync("error", {
-                    type: "errorMessage",
-                    message: error.responseText
-                });
-            });
+
+                
+                  
+                }).fail(errorHandler)
         }
-        function forwardMessage(messageId) {
+        function forwardMessage(messageId,address) {
             var settings = {
                 "url": restUrl + 'messages/' + messageId + '/forward',
                 "type": "POST",
@@ -162,21 +222,17 @@
                     "Content-Type": "application/json",
                     "Authorization": "Bearer " + rawToken
                 },
-                "data": "{ 'ToRecipients': [{'EmailAddress': {'Address': 'amira@codeafterdark.com'}}]}"
+                "data": "{ 'ToRecipients': [{'EmailAddress': {'Address': '" + address +"'}}]}"
 
             }
 
             $.ajax(settings).done(function (response) {
+                showNotification("Message","Message is forwarded successfully.")
                 console.log(response);
-            }).fail(function (error) {
-                console.log(error);
-            });
+            }).fail(errorHandler)
         }
-        function CreatePhishingFolder(messageId, event) {
-            Office.context.mailbox.item.notificationMessages.addAsync("progress", {
-                type: "progressIndicator",
-                message: "Add-in is moving the message to phishing folder."
-            });
+        function CreatePhishingFolder(messageId) {
+            //ShowBarNotification("progress", "progressIndicator", "Add-in is moving the message to phishing folder.");
             var settings = {
                 "url": restUrl + 'mailFolders',
                 "type": "POST",
@@ -188,20 +244,19 @@
             }
 
             $.ajax(settings).done(function (response) {
-                MoveMessageToFolder(messageId, response.Id, event);
+               MoveMessageToFolder(messageId, response.Id);
+              
             }).fail(function (error) {
                 if (error.status == 409) {
                     var url = restUrl + 'mailFolders'
-                    MoveIfExist(url, messageId, event);
+                    MoveIfExist(url, messageId);
                 }
                 else
-                    Office.context.mailbox.item.notificationMessages.addAsync("error", {
-                        type: "errorMessage",
-                        message: error.responseText
-                    });
+                    //ShowBarNotification("error", "errorMessage", error.responseText);
+                    showNotification("error", error.responseText);
             });
         }
-        function MoveMessageToFolder(messageId, folderId, event) {
+        function MoveMessageToFolder(messageId, folderId) {
 
             var settings = {
                 "url": restUrl + 'messages/' + messageId + '/move',
@@ -214,17 +269,10 @@
             }
 
             $.ajax(settings).done(function (response) {
-            }).fail(function (error) {
-                console.log(error);
-
-                Office.context.mailbox.item.notificationMessages.addAsync("error", {
-                    type: "errorMessage",
-                    message: error.responseText
-                });
-            });
+            }).fail(errorHandler)
         }
 
-        function MoveIfExist(url, messageId, event) {
+        function MoveIfExist(url, messageId) {
 
             var settings = {
                 "url": url,
@@ -239,22 +287,46 @@
                     return el.DisplayName;
                 });
                 if ($.inArray("Phishing", foldersNames) == -1)
-                    MoveIfExist(response['@odata.nextLink'], messageId, event);
+                    MoveIfExist(response['@odata.nextLink'], messageId);
                 else {
                     var folderId = response.value[$.inArray("Phishing", foldersNames)].Id;
-                    MoveMessageToFolder(messageId, folderId, event);
+                    MoveMessageToFolder(messageId, folderId);
                 }
-            }).fail(function (error) {
-                console.log(error);
-
-                Office.context.mailbox.item.notificationMessages.addAsync("error", {
-                    type: "errorMessage",
-                    message: error.responseText
-                });
-            });
+            }).fail(errorHandler)
         }
 
 
+        function GenerateLink(href, text, justify) {
+
+            return "<a href='" + href + "' class='text-" + justify + "' > " + text + "</a > ";
+        }
+
+        function GenerateImage(src, alt, justify) {
+            return "<img src='" + src + "' alt='" + alt + "' class='text-" + justify + "'/>";
+        }
+
+        function GenerateText(text, justify) {
+            return "<span class='text-" + justify + "' >" + text + "</span>"; 
+        }
+
+        function GenerateSeparator(lines){ 
+            var Brs = "";
+            for (var i = 0; i < lines; i++) {
+                Brs += "<br/>";
+            }
+            return Brs;
+        }
+        function ShowBarNotification(key,type,message) {
+
+            Office.context.mailbox.item.notificationMessages.addAsync(key
+                , {
+                    type: type,
+                    message: message
+            });
+        }
+        function BarErrorHandler(error) {
+            ShowBarNotification("error", "errorMessage", error.responseText);
+        }
         // Error Handling Region
         function hideErrorMessage() {
 
@@ -265,19 +337,9 @@
         // Helper function for treating errors
         function errorHandler(error) {
             // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
-            switch (error.code) {
-                case "case1":
-                    showNotification("error1", "");
-                    break;
-                default:
-                    showNotification("Error", error);
-                    break;
-            }
-
-            console.log("Error: " + error);
-            if (error instanceof OfficeExtension.Error) {
-                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            }
+            showNotification("Error", error.responseText);
+         
+          
         }
         // Helper function for displaying notifications
         function showNotification(header, content) {
@@ -288,6 +350,33 @@
             hideErrorMessage();
         }
 
+        //Loader
+        var canvas = document.getElementById('spinner');
+        var context = canvas.getContext('2d');
+        var start = new Date();
+        var lines = 16,
+            cW = context.canvas.width,
+            cH = context.canvas.height;
+
+        var draw = function () {
+            var rotation = parseInt(((new Date() - start) / 1000) * lines) / lines;
+            context.save();
+            context.clearRect(0, 0, cW, cH);
+            context.translate(cW / 2, cH / 2);
+            context.rotate(Math.PI * 2 * rotation);
+            for (var i = 0; i < lines; i++) {
+
+                context.beginPath();
+                context.rotate(Math.PI * 2 / lines);
+                context.moveTo(cW / 10, 0);
+                context.lineTo(cW / 4, 0);
+                context.lineWidth = cW / 30;
+                context.strokeStyle = "rgba(0,0,0," + i / lines + ")";
+                context.stroke();
+            }
+            context.restore();
+        };
+        window.setInterval(draw, 1000 / 30);
     });
 
 })();
