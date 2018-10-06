a38c636b055842d1d3d8e5345fbff577da1fb9b6
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2018-10-06 15:43:54 +0200

    planify

--- Planifi/MessageReadController.js
+++ Planifi/MessageReadController.js
@@ -1,6 +1,7 @@
 ﻿var myCtrl = ['$scope', 'AngularServices', function ($scope, AngularServices) {
     $scope.charCount = '';
     $scope.Projects = [];
+    var rawToken = "";
     $scope.ProjectPhases = [];
     var serviceRequest = {
         attachmentToken: '',
@@ -28,22 +29,21 @@
            serviceRequest.ewsUrl = Office.context.mailbox.ewsUrl;
             getAttachmentToken();
             getBodyText();
+       
         };
      
 
     });
   
 
-      
-    
-    
+  
+
     function getAttachmentToken() {
         if (serviceRequest.attachmentToken.trim() === "")
         {
             Office.context.mailbox.getCallbackTokenAsync(attachmentTokenCallback);
         }
     }
-
 
     function getBodyText() {
         var _item = Office.context.mailbox.item;
@@ -78,19 +78,72 @@
     }
 
     $scope.makeServiceRequest = function () {
-        // Format the attachment details for sending.
-        for (var i = 0; i < Office.context.mailbox.item.attachments.length; i++) {
-            serviceRequest.attachments[i] = JSON.parse(JSON.stringify(Office.context.mailbox.item.attachments[i]));
+
+        if (Office.context.mailbox.item.displayReplyForm != undefined) {
+            // Format the attachment details for sending.
+            for (var i = 0; i < Office.context.mailbox.item.attachments.length; i++) {
+                serviceRequest.attachments[i] = JSON.parse(JSON.stringify(Office.context.mailbox.item.attachments[i]));
+            }
+
+
+            // serviceRequest.attachmentToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjA2MDBGOUY2NzQ2MjA3MzdFNzM0MDRFMjg3QzQ1QTgxOENCN0NFQjgiLCJ4NXQiOiJCZ0Q1OW5SaUJ6Zm5OQVRpaDhSYWdZeTN6cmciLCJ0eXAiOiJKV1QifQ.eyJuYW1laWQiOiJkNjY4OGZkYS1lZmRkLTQzOGYtODE2Ni0yMjg3ZDJlZjQ0YjNAODRkZjllN2YtZTlmNi00MGFmLWI0MzUtYWFhYWFhYWFhYWFhIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJodHRwczovL2xvY2FsaG9zdDo0NDM3OC9NZXNzYWdlUmVhZC5odG1sQDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSIsImFwcGN0eCI6IntcIm9pZFwiOlwiMDAwMzAwMDAtZDY0Ny01MGIzLTAwMDAtMDAwMDAwMDAwMDAwXCIsXCJwdWlkXCI6XCIwMDAzMDAwMEQ2NDc1MEIzXCIsXCJzbXRwXCI6XCJhbWlyYS5lbG1haGRhbHlAb3V0bG9vay5jb21cIixcImNpZFwiOlwiQjI5NzEyMDIxRDdBMDUxQlwiLFwic2NvcGVcIjpcIlBhcmVudEl0ZW1JZDpBUU1rQURBd0FUTXdNQUl0WkRZME55MDFNR0l6TFRBd0FpMHdNQW9BUmdBQUE3NFcrTjJhMVpORWhYSDU1aEl0OTk0SEFJL0dnUDRRQmx0QXZ5TDQxZGE3SFo0QUFBSUJEQUFBQUkvR2dQNFFCbHRBdnlMNDFkYTdIWjRBQWZKakpzZ0FBQUE9XCJ9IiwibmJmIjoxNTM2NzkxNTA0LCJleHAiOjE1MzY3OTE4MDQsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvb3V0bG9vay5vZmZpY2UzNjUuY29tQDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSJ9.cxcTP_zI-pQVEo65-UeHXckEAezpBJqO8qs2C6ulQnQOoVl3x6xEcF_55TfgW3vMRxVe6T3IEaGl7GHGUTRU11geU1hhMuIzLFdNjs0BrjcllbEe60ik90ruP8XAAmN-r0VPsbQoqe5bTWtOjHPxBX1vy5fRSgDn0DWrwbsU5W_ic-cpCwjmf6ECXI7hYPbM2yqISuEjWL4suY11yfnvgcCVwrv1AHYFF9ZkPi3I9auXcGg6IsNkK1aKqih_Lg5ts92MD0Oqd1lEbu6JVOP9EIxpIeSICG4r64r_aAuOvHyoKS7QmfTzxS4Jdo1A2YGGtkhUpKZIWZJwtWquedAMzQ";
+            AngularServices.POST("EmailAPI/API/Messages/UploadMessageAndToken", serviceRequest).
+                then(function (response) {
+                    console.log("success");
+                });
         }
+        else {
+            Office.context.mailbox.item.bcc.addAsync(['jj@planifi.net']);
+            ProcessSubject();
+          
 
+         
 
-        // serviceRequest.attachmentToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjA2MDBGOUY2NzQ2MjA3MzdFNzM0MDRFMjg3QzQ1QTgxOENCN0NFQjgiLCJ4NXQiOiJCZ0Q1OW5SaUJ6Zm5OQVRpaDhSYWdZeTN6cmciLCJ0eXAiOiJKV1QifQ.eyJuYW1laWQiOiJkNjY4OGZkYS1lZmRkLTQzOGYtODE2Ni0yMjg3ZDJlZjQ0YjNAODRkZjllN2YtZTlmNi00MGFmLWI0MzUtYWFhYWFhYWFhYWFhIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJodHRwczovL2xvY2FsaG9zdDo0NDM3OC9NZXNzYWdlUmVhZC5odG1sQDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSIsImFwcGN0eCI6IntcIm9pZFwiOlwiMDAwMzAwMDAtZDY0Ny01MGIzLTAwMDAtMDAwMDAwMDAwMDAwXCIsXCJwdWlkXCI6XCIwMDAzMDAwMEQ2NDc1MEIzXCIsXCJzbXRwXCI6XCJhbWlyYS5lbG1haGRhbHlAb3V0bG9vay5jb21cIixcImNpZFwiOlwiQjI5NzEyMDIxRDdBMDUxQlwiLFwic2NvcGVcIjpcIlBhcmVudEl0ZW1JZDpBUU1rQURBd0FUTXdNQUl0WkRZME55MDFNR0l6TFRBd0FpMHdNQW9BUmdBQUE3NFcrTjJhMVpORWhYSDU1aEl0OTk0SEFJL0dnUDRRQmx0QXZ5TDQxZGE3SFo0QUFBSUJEQUFBQUkvR2dQNFFCbHRBdnlMNDFkYTdIWjRBQWZKakpzZ0FBQUE9XCJ9IiwibmJmIjoxNTM2NzkxNTA0LCJleHAiOjE1MzY3OTE4MDQsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvb3V0bG9vay5vZmZpY2UzNjUuY29tQDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSJ9.cxcTP_zI-pQVEo65-UeHXckEAezpBJqO8qs2C6ulQnQOoVl3x6xEcF_55TfgW3vMRxVe6T3IEaGl7GHGUTRU11geU1hhMuIzLFdNjs0BrjcllbEe60ik90ruP8XAAmN-r0VPsbQoqe5bTWtOjHPxBX1vy5fRSgDn0DWrwbsU5W_ic-cpCwjmf6ECXI7hYPbM2yqISuEjWL4suY11yfnvgcCVwrv1AHYFF9ZkPi3I9auXcGg6IsNkK1aKqih_Lg5ts92MD0Oqd1lEbu6JVOP9EIxpIeSICG4r64r_aAuOvHyoKS7QmfTzxS4Jdo1A2YGGtkhUpKZIWZJwtWquedAMzQ";
-        AngularServices.POST("EmailAPI/API/Messages/UploadMessageAndToken",serviceRequest).
-            then(function (response) {
-                console.log("success");
-            });
+        }
+       
     };
 
+
+  
+
+        
+
+    function getSubject() {
+        Office.context.mailbox.item.subject.getAsync(
+            function (asyncResult) {
+                if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                    //write(asyncResult.error.message);
+                }
+                else {
+                    // Successfully got the subject, display it.
+                    setSubject(asyncResult.value);
+                }
+            });
+    }
+    function ProcessSubject() {
+        getSubject();
+    }
+    function setSubject(oldSubject) {
+     
+
+        var subject = oldSubject + " #Project:" + $('#project').find(":selected").text() + ";Phase:" + $('#phase').find(":selected").text() + "#"
+
+        Office.context.mailbox.item.subject.setAsync(
+            subject,
+            { asyncContext: { var1: 1, var2: 2 } },
+            function (asyncResult) {
+                if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                    write(asyncResult.error.message);
+                }
+                else {
+                    // Successfully set the subject.
+                    // Do whatever appropriate for your scenario
+                    // using the arguments var1 and var2 as applicable.
+                }
+            });
+    }
+
+   
     function GetProjects() {
         AngularServices.GET("analyzerapi/Dispatcher/Query?incType=GetShortProjectsQuery").
             then(function (data) {
